b4d2a76ed8fc9321a6a4989728436350f3e3da45
Nickson Rajkarnikar <nicksonrajkarnikar@Nicksons-MacBook-Pro.local>
2018-03-27 19:53:37 +1100

    Nickson/Michelle updated team members

--- client/spec/pages/teamPage.spec.js
+++ client/spec/pages/teamPage.spec.js
@@ -12,12 +12,13 @@ describe('TeamPage', () => {
       const page = new TeamPage();
       expect(page.template()).toContain("<h1>Made by:</h1>");
     });
-
     it('should contain team members names', () => {
-      const page = new TeamPage();
-      expect(page.template()).toContain("Teresa");
-      expect(page.template()).toContain("Harris");
-    });
+    const page = new TeamPage();
+    expect(page.template()).toContain("Teresa");
+    expect(page.template()).toContain("Harris");
+    expect(page.template()).toContain("Nickson");
+    expect(page.template()).toContain("Michelle");
+  });
   });
 
 });
