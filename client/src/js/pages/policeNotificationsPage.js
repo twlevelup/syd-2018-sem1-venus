765b9d01d1292e447e8bf1e5f7b254d61d293103
Nickson Rajkarnikar <nicksonrajkarnikar@Nicksons-MacBook-Pro.local>
2018-04-17 19:35:42 +1000

    [yundi/Nickson] changed the notifications page as per PO

--- client/src/js/pages/policeNotificationsPage.js
+++ client/src/js/pages/policeNotificationsPage.js
@@ -3,7 +3,7 @@ const BasePage = require("watch-framework").BasePage;
 class policeNotificationsPage extends BasePage {
 
   template() {
-    return `<div class="notification">Police have been notified. 😎👮🏻‍</div>`;
+    return `<div class="notification">Police have been notified</div>`;
   }
 
   leftButtonEvent() {
