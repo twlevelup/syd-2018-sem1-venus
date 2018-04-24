656e79d87ccd8bbb4614df4dbb97cde2e20ccd0f
Teresa Pham <teresakypham@gmail.com>
2018-04-24 19:58:53 +1000

    [Nickson/Teresa] Displays ETA and Current Time

--- client/src/js/pages/policeNotificationsPage.js
+++ client/src/js/pages/policeNotificationsPage.js
@@ -1,9 +1,17 @@
 const BasePage = require("watch-framework").BasePage;
 const compiledTemplate = require("../../templates/policeNotificationsPage.hbs");
+const time = new Date(Date.now()).toLocaleString().split(",")[1];
+const diff = 8;
+const eta_time = new Date(Date.now() + diff*60000).toLocaleString().split(",")[1];
 
 class policeNotificationsPage extends BasePage {
   template() {
-    return compiledTemplate();
+    const context = {
+      eta_time: eta_time,
+      time: time
+    };
+
+    return compiledTemplate(context);
   }
 
   leftButtonEvent() {
